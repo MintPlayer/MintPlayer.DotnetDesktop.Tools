0d58c0205dd32030daa768fe149fa44b93b72531
Pieterjan <pieterjandeclippel@msn.com>
2024-12-06 19:32:11 +0100

    Merge repos (#12)

---
+++ QuineMcCluskey/QuineMcCluskey/Data/QuineMcCluskey/Table1/Column.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace QuineMcCluskey.Data.QuineMcCluskey.Table1
+{
+    internal class Column
+    {
+        public Column()
+        {
+            Groups = new List<Group>();
+        }
+
+        public List<Group> Groups { get; set; }
+    }
+}
---
+++ QuineMcCluskey/QuineMcCluskey/Data/QuineMcCluskey/Table1/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace QuineMcCluskey.Data.QuineMcCluskey.Table1
+{
+    internal class Group
+    {
+        public Group()
+        {
+            Records = new List<Loop>();
+        }
+
+        public List<Loop> Records { get; set; }
+    }
+}
